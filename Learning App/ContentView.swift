3eec0c9df5c1918dbd5e3fb35cbbc8ba4d644d6e
FeyisaraO <feyisaraodukoya@gmail.com>
2023-05-29 20:33:56 +0100

    Initial Commit

---
+++ Learning App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning App
+//
+//  Created by Feyisara Odukoya on 29/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Feyisara Odukoya on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
